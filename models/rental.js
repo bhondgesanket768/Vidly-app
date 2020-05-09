3542a7bc93bcf7024c1a9c1775e95a1c5e98f6d2
bhondgesanket768 <bhondgesanket007@gmail.com>
2020-05-09 18:49:37 +0530

    second commit

--- models/rental.js
+++ models/rental.js
@@ -51,7 +51,7 @@ rentalSchema.statics.lookup = function (customerId, movieId) {
 }
 
 rentalSchema.methods.return = function () {
-    this.dateReturned = new date();
+    this.dateReturned = new Date();
     
     const rentalDays = moment().diff(this.dateOut, "days");
     this.rentalFee = rentalDays * this.movie.dailyRentalRate;
